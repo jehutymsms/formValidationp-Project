60fd2d2d9e2750bce8de569ff057a96ab091774b
Andrew Humphries <andrewhumphrieswork@outlook.com>
2022-03-27 08:42:11 -0500

    Basic form validation refactored

--- myscript.js
+++ myscript.js
@@ -1,8 +1,8 @@
-let regex = /^\s*$/
+let regex = /^$/;
 
 const validateField = (name) => {
     let element = document.getElementById(name)
-    if(element.value !== regex){
+    if(regex.test(element.value)){
       errorMessageInsert(element)
       return
     }
@@ -11,12 +11,35 @@ const validateField = (name) => {
 
 const errorMessageInsert = (id) =>{
     let element = id.nextElementSibling
-    element.style.display = 'block'
+    if(element.tagName !== 'P'){
+      let newElement = document.createElement('p')
+    newElement.innerHTML = 'Please Fill in field'
+    id.after(newElement)
     id.classList = 'error'
+    }
 }
 
 const errorMessageRemove = (id) =>{
   let element = id.nextElementSibling
-  element.style.display = 'none'
+  if(element.tagName === 'P'){
+    element.remove()
+  }
   id.classList.remove('error')
 }
+
+const form  = document.getElementById('validationform');
+
+form.addEventListener('submit', (event) => {
+    let newElement = document.createElement('h1')
+    newElement.innerHTML = 'High Five You Filled out the Form'
+    form.after(newElement)
+
+    event.preventDefault();
+});
+
+form.addEventListener('reset', (event) => {
+    let newElement = document.getElementsByTagName('h1')
+    if(newElement[0] !== undefined){
+      newElement[0].remove()
+    }
+});
